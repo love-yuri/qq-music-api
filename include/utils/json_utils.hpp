d761bd676743f00e20c6564d3a1093ed2f2bc6a3
love-yuri <yuri2078170658@gmail.com>
2025-11-25 21:01:30 +0800

    feat: 优化json反序列化

--- include/utils/json_utils.hpp
+++ include/utils/json_utils.hpp
@@ -1,16 +1,35 @@
 #pragma once
 
+#include <expected>
 #include <string>
 #include "glaze/glaze.hpp"
 #include "yuri_log.hpp"
 
+/**
+ * 解析选项
+ */
+constexpr glz::opts options = {
+  .comments = true,               // 允许包含注释
+  .error_on_unknown_keys = false, // 忽略未知键
+};
+
+template <glz::read_supported<glz::JSON> T, glz::is_buffer Buffer>
+[[nodiscard]] glz::expected<T, glz::error_ctx> read_with_options(Buffer &&buffer) {
+  T value{};
+  glz::context ctx{};
+  if (const glz::error_ctx ec = read<options>(value, std::forward<Buffer>(buffer), ctx)) {
+    return glz::unexpected(ec);
+  }
+  return value;
+}
+
 template <typename T>
-T read_json(std::string_view json) {
-  const auto res = glz::read_json<T>(json);
+T read_json(const std::string_view json) {
+  const auto res = read_with_options<T>(json);
   if (res) {
     return res.value();
   }
 
   yerror << "json反序列化失败：" << glz::format_error(res, json);
-  return T {};
+  return T{};
 }
