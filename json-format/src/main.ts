850e8178aa7f779c45bda889bddc8de37e5cb6c8
love-yuri <yuri2078170658@gmail.com>
2025-11-26 21:24:58 +0800

    feat: 新增json格式化工具，支持一键json格式化并转为struct

---
+++ json-format/src/main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: love-yuri yuri2078170658@gmail.com
+ * @Date: 2025-11-26 20:41:01
+ * @LastEditTime: 2025-11-26 20:44:41
+ * @Description: 
+ */
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
